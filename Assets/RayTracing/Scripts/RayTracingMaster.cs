9a0783d8e90dadce63b0e6eccf94cb3a97a41833
mollyxxl <1187820970@qq.com>
2020-07-21 17:21:04 +0800

    Init Ray Tracing Project

---
+++ Assets/RayTracing/Scripts/RayTracingMaster.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+    public ComputeShader rayTracingShader;
+    private RenderTexture _target;
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Render(destination);
+    }
+
+    private void Render(RenderTexture destination)
+    {
+        InitRenderTexture();
+
+        //Set the target and dispatch the compute shader
+        rayTracingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        rayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        //Blit the result texture to the screen
+        Graphics.Blit(_target, destination);
+    }
+
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            //release old render texture
+            if (_target != null)
+                _target.Release();
+
+            _target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
+    }
+}
